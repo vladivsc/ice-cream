550b61918b5f7de734701816c9fb95ca9a173435
Volodymyr Dudka <tanetti22@gmail.com>
2022-06-25 12:53:12 +0300

    Add header blur features

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -12,7 +12,12 @@
     refs.mobileMenuBtn.setAttribute('aria-expanded', !expanded);
 
     refs.mobileMenuBtn.classList.toggle('is-open');
+
     refs.mobileMenu.classList.toggle('is-open');
+    if (refs.mobileMenu.classList.contains('is-open')) {
+      refs.mobileMenu.scrollTo(top);
+    }
+
     refs.buyBtn.classList.toggle('disabled');
 
     document.body.classList.toggle('mobile-menu-is-open');
