8709f52c5ba5e37952a628a7342cbcb00f6c4fb0
Volodymyr Dudka <tanetti22@gmail.com>
2022-06-28 00:33:19 +0300

    Tablet & Desctop done

---
+++ src/js/hero-more.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    heroAboutBtn: document.querySelector('[data-hero-about-button]'),
+    heroAboutTxt: document.querySelector('[data-hero-about-text]'),
+    heroAboutDesc: document.querySelector('[data-hero-about-description]'),
+  };
+
+  refs.heroAboutBtn.addEventListener('mouseover', () => {
+    refs.heroAboutTxt.classList.add('is-text-invisible');
+    refs.heroAboutDesc.classList.add('is-description-visible');
+    refs.heroAboutBtn.classList.add('is-about-button-hovered');
+  });
+  refs.heroAboutBtn.addEventListener('mouseout', () => {
+    refs.heroAboutTxt.classList.remove('is-text-invisible');
+    refs.heroAboutDesc.classList.remove('is-description-visible');
+    refs.heroAboutBtn.classList.remove('is-about-button-hovered');
+  });
+})();
