4cab4b6a6837e7e088cf6328bc3fcd3bc0f84976
Volodymyr Dudka <tanetti22@gmail.com>
2022-06-29 20:47:22 +0300

    fix gallery and add active menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,6 +4,7 @@
     mobileMenu: document.querySelector('[data-mobile-menu]'),
     mobileMenuList: document.querySelector('[data-mobile-menu-list]'),
     buyBtn: document.querySelector('[data-buy-button]'),
+    mobileBuyBtn: document.querySelector('[data-mobile-menu-buy]'),
   };
 
   refs.mobileMenuBtn.addEventListener('click', () => {
@@ -22,6 +23,16 @@
   });
 
   refs.mobileMenuList.addEventListener('click', () => {
+    const expanded =
+      refs.mobileMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.mobileMenuBtn.setAttribute('aria-expanded', !expanded);
+
+    refs.mobileMenuBtn.classList.toggle('is-open');
+    refs.mobileMenu.classList.toggle('is-open');
+    refs.buyBtn.classList.toggle('disabled');
+  });
+
+  refs.mobileBuyBtn.addEventListener('click', () => {
     const expanded =
       refs.mobileMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     refs.mobileMenuBtn.setAttribute('aria-expanded', !expanded);
